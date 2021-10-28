1c9dab64326a011aa90110b99957fa90a250de3d
kouhevil <kouhevil@gmail.com>
2021-10-28 03:02:52 +0200

    modif bdd

--- formulaire.php
+++ formulaire.php
@@ -2,7 +2,7 @@
 require 'db-config.php';
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password, $db);
+    $conn = new PDO("mysql:host=$servername;dbname=$db", $username, $password, $db);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
