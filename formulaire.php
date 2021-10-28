754f417b52f1826b99a20e6117cc59b6420f5a3c
kouhevil <kouhevil@gmail.com>
2021-10-28 10:48:51 +0200

    modifs

--- formulaire.php
+++ formulaire.php
@@ -213,7 +213,7 @@ try {
                 </div>
 
                 <div class="mb-3 form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" id="socMat" name="socMat" value="true" disabled>
+                    <input class="form-check-input" type="checkbox" id="socMat" name="socMat" value="true">
                     <label class="form-check-label" for="socMat">Cochez cette case si vous faites partir des sociétaires de la MATMUT</label>
                 </div>
 
